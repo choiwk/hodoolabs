51de136ab45d05faca0e863ed576d50ef06584f3
abg3000 <abg3000@naver.com>
2022-02-14 17:44:19 +0900

    [Docs] eslint 설정

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,10 @@
+import React from 'react';
 import type { NextPage } from 'next';
 import Head from 'next/head';
 import Image from 'next/image';
 
 const Home: NextPage = () => {
-  return <></>;
+  return <div>안녕</div>;
 };
 
 export default Home;
