b6ed3b8aa91d60a05097cee44a1e4a7686129f00
WY K <ablack1029@gmail.com>
2022-02-15 22:22:13 +0900

    Feat : 땅콩스쿨 배너 UI

--- components/Compo.tsx
+++ components/Compo.tsx
@@ -1,6 +1,6 @@
+import { useState, useEffect } from "react";
 import styles from "./Compo.module.scss";
 import Image from "next/image";
-import { useState, useEffect } from "react";
 
 export default function Compo() {
   const [position, setPosition] = useState(0);
@@ -18,23 +18,42 @@ export default function Compo() {
 
   return (
     <div className={styles.wrap}>
-      <div className={styles.img}>
-        <Image
-          alt='logo'
-          src='/public/image/logo.png'
-          width='94px'
-          height='60px'
-        />
-      </div>
-      <h1 className={styles.title} style={{ opacity: (position - 300) / 10 }}>
-        땅콩스쿨이란?
-      </h1>
-      <div className={styles.sub}>
-        <p>실시간(LIVE)으로, 아이들이 좋아하는</p> <p>캐릭터 선생님을 통해,</p>
-      </div>
-      <div className={styles.sub}>
-        <p>친구들과 함께 창의독서, 퀴즈 등을</p>
-        <p> 재미있게 즐길 수 있는 온라인 서비스입니다.</p>
+      <div className={styles.textWrap}>
+        <div className={styles.img}>
+          <Image
+            alt='logo'
+            src='/public/image/logo.png'
+            width='94px'
+            height='60px'
+          />
+        </div>
+        <div>
+          <div className={styles.titleWrap}>
+            <h1
+              className={styles.title}
+              style={{
+                opacity: (position - 500) / 200,
+              }}
+            >
+              땅콩스쿨이란?
+            </h1>
+          </div>
+        </div>
+        <div
+          className={styles.subtitle}
+          style={{
+            opacity: (position - 600) / 200,
+          }}
+        >
+          <div className={styles.sub}>
+            <p>실시간(LIVE)으로, 아이들이 좋아하는</p>{" "}
+            <p>캐릭터 선생님을 통해,</p>
+          </div>
+          <div className={styles.sub}>
+            <p>친구들과 함께 창의독서, 퀴즈 등을</p>
+            <p> 재미있게 즐길 수 있는 온라인 서비스입니다.</p>
+          </div>
+        </div>
       </div>
     </div>
   );
