f0f11d656df12de1a7766515bc874390b275a6b1
WY K <ablack1029@gmail.com>
2022-02-16 00:24:51 +0900

    Feat : animation 구현

--- components/Compo.tsx
+++ components/Compo.tsx
@@ -1,6 +1,7 @@
 import { useState, useEffect } from "react";
 import styles from "./Compo.module.scss";
 import Image from "next/image";
+import classNames from "classnames";
 
 export default function Compo() {
   const [position, setPosition] = useState(0);
@@ -18,32 +19,15 @@ export default function Compo() {
 
   return (
     <div className={styles.wrap}>
-      <div className={styles.textWrap}>
-        <div className={styles.img}>
-          <Image
-            alt='logo'
-            src='/public/image/logo.png'
-            width='94px'
-            height='60px'
-          />
+      <div>
+        <div className={position > 250 ? styles.img : styles.imghidden}>
+          <Image alt='logo' src='/images/logo.png' width='94px' height='60px' />
         </div>
-        <div>
-          <div className={styles.titleWrap}>
-            <h1
-              className={styles.title}
-              style={{
-                opacity: (position - 500) / 200,
-              }}
-            >
-              땅콩스쿨이란?
-            </h1>
-          </div>
+        <div className={position > 280 ? styles.title : styles.titlehidden}>
+          <div>땅콩스쿨이란?</div>
         </div>
         <div
-          className={styles.subtitle}
-          style={{
-            opacity: (position - 600) / 200,
-          }}
+          className={position > 380 ? styles.subTitle : styles.subTitlehidden}
         >
           <div className={styles.sub}>
             <p>실시간(LIVE)으로, 아이들이 좋아하는</p>{" "}
